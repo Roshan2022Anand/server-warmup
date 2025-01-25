2ca4681f0a9ab39f2cef4841b496db954040f8d5
unknown <roshananand2005@gmail.com>
2025-01-25 11:42:24 +0530

    created a function that warmups the server every 25 min

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module server-warmup
 
 go 1.23.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -2,16 +2,54 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
 )
 
+// to check if the server is up
 func homeRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Home Page")
-	log.Printf("home route")
+	fmt.Fprintf(w, "Server is up and running")
+}
+
+// to warm up the server
+func warmUpServer() {
+	backendURL := os.Getenv("WARMUP_SERVER_URL")
+	res, err := http.Get(backendURL)
+	if err != nil {
+		log.Printf("Error while making request to backend server %s\n", err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("Error while reading response body %s\n", err)
+		return
+	}
+	log.Printf(" %s\n", body)
 }
 
 func main() {
-	http.HandleFunc("/", homeRoute)
+
+	//load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	//to call warmUpServer func every 25 minutes
+	go func() {
+		for {
+			warmUpServer()
+			time.Sleep(25 * time.Minute)
+		}
+	}()
+
+	http.HandleFunc("/", homeRoute) //route to check if the server is up
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
